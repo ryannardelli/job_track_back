c7beb639e865c371aec3687d6f6c79844905b706
Ryan Nardelli <ryannardelli12@gmail.com>
2026-06-12 14:40:38 -0300

    chore: Implemented new fix to config database

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,38 +1,17 @@
-// require("dotenv").config();
-
-// const { Sequelize } = require("sequelize");
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect: "postgres",
-//     logging: false,
-//   }
-// );
-
-// module.exports = sequelize;
-
 const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
 
 const { Sequelize } = require("sequelize");
 
-const env = process.env.NODE_ENV || "development";
-
-if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 let sequelize;
 
-if (env === "production") {
+if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    protocol: "postgres",
     logging: false,
     dialectOptions: {
       ssl: {
